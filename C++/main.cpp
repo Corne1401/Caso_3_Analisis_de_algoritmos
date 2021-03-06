8cf42c7321da6c04a26494af8d84f254da17fde7
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-03-05 19:04:14 -0600

    c++ code

---
+++ C++/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a[100], n, i, j, position, swap;
+
+    cout << "Enter number of elements" << endl;
+    scanf("%d", &n);
+    cout << "Enter "<< n << "Numbers" << endl;
+
+    for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+    for(i = 0; i < n - 1; i++){
+        position=i;
+    }
+    for(j = i + 1; j < n; j++){
+        if(a[position] > a[j]){
+            position=j;
+        }
+        if(position != i){
+            swap=a[i];
+            a[i]=a[position];
+            a[position]=swap;
+        }
+    }
+
+    cout << "Sorted Array: " << endl;
+    for(i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+}
