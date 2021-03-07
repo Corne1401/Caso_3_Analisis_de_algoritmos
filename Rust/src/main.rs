8557d34aca1357ce27842e02f04056f955ee9292
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-03-07 12:27:14 -0600

    Rust with time stamp

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,34 +1,60 @@
 use std::io;
 use std::io::*;
 use rand::Rng;
+use std::time::{Duration, Instant};
 
 fn main() {
     let mut _a: [u32; 100000] = [0; 100000];
     let mut rng = rand::thread_rng();
 
     let mut _n = 0;
-    let _j = 0;
-    let _position = 0;
+    let mut position;
+    let mut swap;
 
-    println!("Enter number of elements: ");
-    
+    //input para definir el tamaño del array
+    println!("ingrese numero de elementos(100000 max): "); 
     let mut input_text = String::new();
-    io::stdin()
-        .read_line(&mut input_text)
-        .expect("failed to read from stdin");
+    io::stdin().read_line(&mut input_text).expect("failed to read from stdin");
 
+    //conversion del string del input a un numero entero
     let trimmed = input_text.trim();
     match trimmed.parse::<u32>() {
         Ok(i) => _n = i,
         Err(..) => println!("this was not an integer: {}", trimmed),
     };
 
+    //se llena el array con numeros random
     for index in 0.._n{
         _a[index as usize] = rng.gen::<u32>();
     }
 
-    for element in 0.._n{
-        println!("{}",_a[element as usize])
-    }
+    let start = Instant::now();
+
+    for i in 0.._n+1{
     
+        position = i;
+        //busca element mas pequeño del array
+        for j in i+1.._n{
+            if _a[position as usize] > _a[j as usize]{
+                position = j;
+            }
+        }
+        //revisa si hay que hacer un swap
+        if position != i{
+            swap = _a[i as usize];
+            _a[i as usize] = _a[position as usize];
+            _a[position as usize] = swap;
+            
+        }
+
+    }
+
+    let duration = start.elapsed();
+
+    println!("El tiempo de ejecucion fue de: {:?}", duration);
+
+    //imprime el array ordenado
+    //for x in 0.._n{ println!("{}",_a[x as usize]); }
+    
+
 }
