38bf19988b1b7d710ab9ee37bca051d6984408ed
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-03-07 12:41:23 -0600

    c++ time stamp added

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int main(){
-    int a[100], n, i, j, position, swap;
+    int a[100000], n, i, j, position, swap;
 
     cout << "Enter number of elements" << endl;
     scanf("%d", &n);
-    cout << "Enter "<< n << "Numbers" << endl;
 
-    for (i = 0; i < n; i++)
-    scanf("%d", &a[i]);
+    for (i = 0; i < n; i++){
+        a[i] = (unsigned)rand();
+    }
+
+    clock_t tStart;
+    tStart = clock();
+
+    
     for(i = 0; i < n - 1; i++){
         position=i;
-    }
-    for(j = i + 1; j < n; j++){
-        if(a[position] > a[j]){
-            position=j;
+        for(j = i + 1; j < n; j++){
+            if(a[position] > a[j]){
+                position=j;
+            }
         }
         if(position != i){
-            swap=a[i];
-            a[i]=a[position];
-            a[position]=swap;
-        }
+                swap=a[i];
+                a[i]=a[position];
+                a[position]=swap;
+            }
     }
+    
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-    cout << "Sorted Array: " << endl;
-    for(i = 0; i < n; i++){
-        cout << a[i] << " ";
-    }
+    // cout << "Sorted Array: " << endl;
+    // for(i = 0; i < n; i++){
+    //     cout << a[i] << endl;
+    // }
     
     return 0;
 }
