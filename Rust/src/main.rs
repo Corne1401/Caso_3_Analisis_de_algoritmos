d1775038e2f40e51794199467cc7634d6c223482
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-03-07 11:51:05 -0600

    Rust ver 2

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -3,16 +3,32 @@ use std::io::*;
 use rand::Rng;
 
 fn main() {
-    let _a: [i32; 1000000] = [0; 1000000];
-    let mut _random_generator = rand::thread_rng();
+    let mut _a: [u32; 100000] = [0; 100000];
+    let mut rng = rand::thread_rng();
 
-    let _n;
+    let mut _n = 0;
     let _j = 0;
     let _position = 0;
 
     println!("Enter number of elements: ");
     
-    
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
 
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => _n = i,
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+
+    for index in 0.._n{
+        _a[index as usize] = rng.gen::<u32>();
+    }
+
+    for element in 0.._n{
+        println!("{}",_a[element as usize])
+    }
     
 }
---
+++ Rust/target/debug/deps/rand_chacha-7a60f8a509892ab0.d
@@ -0,0 +1,9 @@
+C:\Users\Daniel\Documents\TEC\Analisis de algoritmos\vs projects\Casos\Caso3\Caso_3_Analisis_de_algoritmos\Rust\target\debug\deps\rand_chacha-7a60f8a509892ab0.rmeta: C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs
+
+C:\Users\Daniel\Documents\TEC\Analisis de algoritmos\vs projects\Casos\Caso3\Caso_3_Analisis_de_algoritmos\Rust\target\debug\deps\librand_chacha-7a60f8a509892ab0.rlib: C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs
+
+C:\Users\Daniel\Documents\TEC\Analisis de algoritmos\vs projects\Casos\Caso3\Caso_3_Analisis_de_algoritmos\Rust\target\debug\deps\rand_chacha-7a60f8a509892ab0.d: C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs
+
+C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs:
+C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs:
+C:\Users\Daniel\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs:
